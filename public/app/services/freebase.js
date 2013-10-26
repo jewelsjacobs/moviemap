d5338b3ee43ede499a5631952eec12788b925627
Julia Jacobs <julia_jacobs@labs.att.com>
2013-10-26 17:10:47 -0500

    fixed CORS freebase issue

--- public/app/services/freebase.js
+++ public/app/services/freebase.js
@@ -7,22 +7,25 @@
 angular.module('movieMapApp')
 	.service('FreebaseAPI', [ '$http', '$q', '$rootScope', '$location', 'promiseTracker', function ($http, $q, $rootScope, $location, promiseTracker) {
 		return {
-			get: function (params) {
+			get: function (query) {
 				$rootScope.loading = promiseTracker('loading');
 
+				var API_KEY = 'AIzaSyCB6HFuNF-E9qTHW9Ba39NgkpHx701gr1Q';
+				var FREEBASE_API_URL = 'https://www.googleapis.com/freebase/v1/search';
+				var FREEBASE_API_MAX_RESULTS = 30;
+
 				var config = {
-					method: 'GET',
-					url: 'https://www.googleapis.com/freebase/v1/search',
-					params: params,
-					headers: {
-						'contentType': false
-					},
-					tracker: 'loading'
+					params: {
+						query: query,
+						key: API_KEY,
+						limit: FREEBASE_API_MAX_RESULTS,
+						callback: 'JSON_CALLBACK'
+					}
 				};
 
 				var deferred = $q.defer();
 
-				$http(config).
+				$http.jsonp(FREEBASE_API_URL, config).
 					success(function (data) {
 						deferred.resolve(data);
 						console.log('Successful FreebaseAPI response!', data);
